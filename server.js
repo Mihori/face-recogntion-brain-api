e16530112acbaed4f50311159838ff5ad43c004a
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-12 20:46:07 +0100

    chore(): set knex

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'gabordobrozemszky',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('working');
+});
+
+app.post('/signin', (req, res) => {
+  
 });
 
 app.listen(3000, () => {
