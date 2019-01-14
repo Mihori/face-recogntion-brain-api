3fabcd55ba1783e874914a8ea095ade3febd1c9a
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-14 15:08:15 +0100

    chore(): update database host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-adjacent-70268',
     user : 'gabordobrozemszky',
     password : '',
     database : 'smart-brain'
